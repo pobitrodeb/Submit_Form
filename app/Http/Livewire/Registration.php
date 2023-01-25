2d33a74f312b7ccb6213fce764891e5de8102016
Pobitrodeb <pobitrodn@gmail.com>
2023-01-25 23:25:21 +0600

    Submit Form In Livewire done successfully

--- app/Http/Livewire/Registration.php
+++ app/Http/Livewire/Registration.php
@@ -6,8 +6,16 @@ use Livewire\Component;
 
 class Registration extends Component
 {
+    public $name;
+    public $roll_number;
+    public $email;
     public function render()
     {
         return view('livewire.registration');
+    }
+
+    public function submit()
+    {
+        dd($this->name, $this->roll_number, $this->email);
     }
 }
--- resources/views/livewire/registration.blade.php
+++ resources/views/livewire/registration.blade.php
@@ -1,3 +1,14 @@
 <div>
-    {{-- In work, do what you enjoy. --}}
+
+    <div class="container p-4">
+        <h1 class="text-center"> Student Register Form </h1>
+
+        <form wire:submit.prevent="submit">
+            <input type="text" class="form-control my-2" placeholder="Enter Name " wire:model="name">
+            <input type="number" class="form-control my-2" placeholder="Enter Roll Number" wire:model="roll_number">
+            <input type="email" class="form-control my-2" placeholder="Enter Email Address" wire:model="email">
+            <button type="submit" class="btn btn-primary"> Save </button>
+        </form>
+    </div>
+
 </div>
